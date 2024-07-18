b4c2dad2c9c6237f8fede85c76902408b43ed2f0
OWUSU-web <99048088+OWUSU-web@users.noreply.github.com>
2024-07-18 01:58:50 +0000

    first commit

---
+++ .history/components/NavFavourites_20230718233403.js
@@ -0,0 +1,47 @@
+import { FlatList, TouchableOpacity, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Icon } from 'react-native-elements';
+import tw from 'tailwind-react-native-classnames';
+
+const data = [
+  {
+    id: "123",
+    icon: "myself",
+    location: "book for yourself",
+  },
+  {
+    id: "456",
+    icon: "friends",
+    location: "book for someone",
+  },
+];
+
+const NavFavourites = () => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={() => (
+        <View style={[tw`bg-gray-200 m-1`, { height: 0.5 }]} />
+      )}
+      renderItem={({ item: { location, icon } }) => (
+        <TouchableOpacity style={tw`flex-row items-center p-5`}>
+          <Icon
+            style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+            name={icon}
+            type="ionicon"
+            color="white"
+            size={18}
+          />
+          <View>
+            <Text style={tw`font-semibold text-lg`}>{location}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavFavourites;
+
+const styles = StyleSheet.create({});
